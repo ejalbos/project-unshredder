bde5f57874303995969ae1846dca67aa6dfabba5
Bill Selig <uwash@ejalbos.com>
2011-11-18 07:52:02 -0800

    using grayscale for diffs now

--- lib/image_slice.rb
+++ lib/image_slice.rb
@@ -1,3 +1,5 @@
+require 'chunky_png'
+
 class ImageSlice
   attr_reader :left_col
   attr_reader :slice_number
@@ -13,23 +15,27 @@ class ImageSlice
     @neighbor_info = []
   end
   
+  def self.pixel_diff(a, b)
+    (ChunkyPNG::Color.grayscale_teint(a) - ChunkyPNG::Color.grayscale_teint(b)).abs
+  end
+  
   def self.calculate_column_diff(left_col, right_col)
     diff = 0
     left_col.each_with_index do |val, idx|
-      diff += (val - right_col[idx]).abs
+      diff += pixel_diff(val, right_col[idx])
     end
     diff
   end
   
   NeighborInfo = Struct.new(:slice_number, :diff)
   
-  def analyze_right_left_matches(other_slices)
+  def analyze_right_left_matches(other_slices, verbose = nil)
     # find all the diffs compared to each of the others
-    puts "--------------------- analyze_right_left_matches for slice #{slice_number}"
+    puts "--------------------- analyze_right_left_matches for slice #{slice_number}" if verbose
     other_slices.each do |other|
       total_diff = self.class.calculate_column_diff @right_col, other.left_col
       @neighbor_info << NeighborInfo.new(other.slice_number, total_diff)
-      puts sprintf "  %2d %15d", other.slice_number, total_diff
+      puts sprintf "  %2d %15d", other.slice_number, total_diff if verbose
     end
     # now find the likely next slice
     @average_neighbor_diff = 0
