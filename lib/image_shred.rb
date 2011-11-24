123918368571db7ee31c83a9d30f0463dde86502
Bill Selig <uwash@ejalbos.com>
2011-11-24 12:07:48 -0800

    filename arg processing

--- lib/image_shred.rb
+++ lib/image_shred.rb
@@ -8,11 +8,21 @@ class ImageShred
     
 end
 
-p "Program #{$PROGRAM_NAME} called."
-p "Args..."
-ARGV.each do|a|
-  puts "Argument: #{a}"
+#p "Program #{$PROGRAM_NAME} called."
+#p "Args..."
+#ARGV.each do|a|
+#  puts "Argument: #{a}"
+#end
+#p "...done"
+
+filename = ARGV[0]
+unless filename
+  p "--- Image Shredder --- you must specify a filename to shred"
+  p "  try: 'rake shred[filename]'"
+  p "   or: 'bundle exec ruby lib/image_shred.rb filename'"
+  exit
 end
-p "...done"
+
+is = ImageShred.new filename
 
 exit # DEBUG
