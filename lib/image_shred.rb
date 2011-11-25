8e8770e34e40b43382822fc6b9f84d69bd002aef
Bill Selig <uwash@ejalbos.com>
2011-11-24 19:11:19 -0800

    determining shredding slice size

--- lib/image_shred.rb
+++ lib/image_shred.rb
@@ -4,8 +4,51 @@ require_relative 'image_slice'
 class ImageShred
   def initialize(filename)
     @fname_orig = filename
+    puts "Reading in '#{filename}'..."
     @img = ChunkyPNG::Image.from_file(filename)
+    puts "... input done."
   end
+  
+  def process
+    puts "Processing image..."
+    dim = @img.dimension
+    puts "- image is #{dim.width}x#{dim.height} pixels"
+    num_slices, slice_width = determine_slice_params dim.width
+    puts "- will shred into #{num_slices} slices, each #{slice_width} pixels wide"
+    puts "TBD"
+    puts "... processing done."
+  end
+  
+  def output
+    new_fname = @fname_orig.sub(/\.png\Z/, "_shredded.png")
+    puts "Outputing image '#{new_fname}'..."
+    puts "TBD"
+    puts "... output done."
+  end
+private
+  
+  def determine_slice_params(total_width)
+    # try for about 20 slices, go larger or smaller depending on image size
+    size_ref = 35
+    slice_count_ref = 20
+    size = total_width / slice_count_ref
+    if total_width % slice_count_ref == 0
+      return slice_count_ref, size
+    else
+      if size < size_ref
+        (slice_count_ref-1).downto(10).each do |count|
+          return count, total_width / count if total_width % count == 0
+        end
+        raise "Unable to determine slice size downward"
+      else
+        (slice_count_ref+1).upto(40).each do |count|
+          return count, total_width / count if total_width % count == 0
+        end
+        raise "Unable to determine slice size uwpard"
+      end
+    end
+  end
+  
 end
 
 puts "--- Image Shredder ---"
